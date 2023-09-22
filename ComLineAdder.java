8fecc3d50e53a01974bc03a22d94f56e21807758
zharia gardner <zgardner@xula.edu>
2023-09-22 14:34:21 +0000

    added count

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,14 +1,16 @@
 public class ComLineAdder {
     public static void main(String[] args) {
+      int count = 0;
         int sum = 0;
         System.out.printf("%d arguments\n",args.length);
         for(int ndx = 0; ndx < args.length; ndx++) {
             try {
                 sum += Integer.parseInt(args[ndx]);
+              count += 1;
             } catch (Exception e) {
                 System.out.printf("Couldn't convert %s\n",args[ndx]);
             }       
         }
-        System.out.printf("Total of %d values is: %,d\n",args.length,sum);
+        System.out.printf("Total of %d values is: %,d\n",args.length,sum,count);
     }
 }
